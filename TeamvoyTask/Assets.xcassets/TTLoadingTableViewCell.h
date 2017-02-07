49787f5f7cb3cc4742603587f155398e141471e9
Stepan Paholyk <stepanpaholyk@gmail.com>
2017-02-08 00:12:25 +0200

    Add like image

---
+++ TeamvoyTask/Assets.xcassets/TTLoadingTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  TTLoadingTableViewCell.h
+//  TeamvoyTask
+//
+//  Created by Stepan Paholyk on 2/7/17.
+//  Copyright © 2017 Stepan Paholyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTLoadingTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *indicator;
+
+@end
---
+++ TeamvoyTask/Assets.xcassets/TTLoadingTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TTLoadingTableViewCell.m
+//  TeamvoyTask
+//
+//  Created by Stepan Paholyk on 2/7/17.
+//  Copyright © 2017 Stepan Paholyk. All rights reserved.
+//
+
+#import "TTLoadingTableViewCell.h"
+
+@implementation TTLoadingTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
